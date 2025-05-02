1c4ddafee98024f92df69375f490c68ba471e9e4
jaowens14 <jaowens14@protonmail.com>
2025-05-02 14:34:59 -0600

    Working Ext_Encoder.hpp

--- src/Ext_Encoder.hpp
+++ src/Ext_Encoder.hpp
@@ -2,13 +2,9 @@
 #define EXTERNAL_ENCODER
 
 #include <Arduino.h>
-#include "stm32h7xx_hal.h"
-#include "stm32h7xx_hal_gpio.h"
 
-#define PIN_PHASE_A GPIO_PIN_9
-#define PIN_PHASE_B GPIO_PIN_10
-
-#define ENCODER_GPIO_PORT GPIOH
+#define pinA A5
+#define pinB A6
 
 class ExtEncoder {
     public: 
@@ -16,33 +12,11 @@ class ExtEncoder {
         volatile long position;
         volatile int lastEncoded;
         volatile bool indexDetected;
-  
-        const uint8_t pinA = PH_9; 
-        const uint8_t pinB = PH_10;
-        // const uint8_t pinZ = D4;
+
 
         ExtEncoder(): position(0), lastEncoded(0), indexDetected(false) { }
 
         void setup() {
-            
-            /*
-            HAL_Init();
-
-            __HAL_RCC_GPIOH_CLK_ENABLE();
-
-            GPIO_InitTypeDef GPIO_InitStruct = {0};
-
-            GPIO_InitStruct.Pin = GPIO_PIN_9;
-            GPIO_InitStruct.Pin = GPIO_PIN_10;
-            GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-            GPIO_InitStruct.Pull = GPIO_PULLUP;
-            GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-
-            HAL_GPIO_DeInit(GPIOH, GPIO_PIN_9);
-            HAL_GPIO_DeInit(GPIOH, GPIO_PIN_10);
-
-            HAL_GPIO_Init(GPIOH, &GPIO_InitStruct);
-            */
 
             pinMode(pinA, INPUT_PULLUP);
             pinMode(pinB, INPUT_PULLUP);
@@ -58,8 +32,8 @@ class ExtEncoder {
         }
 
         int readEncoderPins() {
-            int a = HAL_GPIO_ReadPin(ENCODER_GPIO_PORT, PIN_PHASE_A);
-            int b = HAL_GPIO_ReadPin(ENCODER_GPIO_PORT, PIN_PHASE_B);
+            int a = digitalRead(pinA);
+            int b = digitalRead(pinB);
             return (a << 1) | b;
         }
 
@@ -91,7 +65,7 @@ class ExtEncoder {
         void debugOutput() {
             Serial.print("Encoder Position: ");
             Serial.print(getPosition());
-            Serial.print(digitalRead(PH_9));
+            //Serial.print(digitalRead(pinA));
             delay(100);
             if(indexDetected) {
                 Serial.print(" (Index pulse detected!)");
@@ -109,9 +83,11 @@ class ExtEncoder {
             Serial.print(digitalRead(pinA));
             Serial.print(" Pin B: ");
             Serial.println(digitalRead(pinB));
+            delay(200);
         }   
 
         static ExtEncoder* _instance;
+  // put your main code here, to run repeatedly:
 
         static void isrA() {
             if (_instance) _instance->updateEncoder();
--- src/main.cpp
+++ src/main.cpp
@@ -27,8 +27,8 @@ Ultrasonic ultrasonic;
 //#include <Encoder.hpp>
 //Encoder encoder(3, 0x6064);
 
-//#include <Ext_Encoder.hpp>
-//ExtEncoder encoder;
+#include <Ext_Encoder.hpp>
+ExtEncoder encoder;
 
 #include <Actuator.hpp>
 ActuatorControl actuator;
@@ -76,119 +76,6 @@ void m7timer() {
 }
 Portenta_H7_Timer M7Timer(TIM7);
 
-/*
- //Serial command processor for actuator test
-void processSerialCommands() {
-  //Use a static budder to persist data between calls.
-    static String commandBuffer = "";
-
-    //Process available characters in the serial input
-    while (Serial.available() > 0) {
-      char c = Serial.read();
-      commandBuffer += c;
-    }
-
-    // Process every complete command (delimited by '/n') into the buffer
-    int newlineIndex = commandBuffer.indexOf('\n');
-    while (newlineIndex != -1) {
-      //Extract one command (up to the newline) and trim whitespace.
-      String currentCommand = commandBuffer.substring(0,newlineIndex);
-      currentCommand.trim();
-
-      //Remove the processed command from the buffer
-      commandBuffer = commandBuffer.substring(newlineIndex + 1);
-
-      //Skip empty commands.
-      if (currentCommand.length() == 0) {
-        newlineIndex = commandBuffer.indexOf('\n');
-        continue;
-      }
-
-      //Ignore incoming heartbeat JSON messages
-      if (currentCommand.startsWith("<{") && currentCommand.endsWith("}>")) {
-        if (currentCommand.indexOf("\"hb\"") != -1 ) {
-          newlineIndex = commandBuffer.indexOf('\n');
-          continue;
-        }
-      }
-
-        Serial.print("Received command: ");
-        Serial.println(currentCommand);
-
-        if(currentCommand.equalsIgnoreCase("help")) {
-            Serial.println("Commands:");
-            Serial.println(" help            - Show this help message");
-            Serial.println(" state           - Print current actuator state");
-            Serial.println(" sdo <node> <index>        - Send an SDO read request for the given CANopen node and object index");
-            Serial.println("                            e.g., sdo 1 0x6064 (to read 'Position actual value')");
-            Serial.println(" test            - Run self-test routine");
-            Serial.println(" set <n> <v>     - Set actuator n (0-indexed) to voltage v (0-5V)");
-        }
-        else if (currentCommand.equalsIgnoreCase("state")) {
-            actuator.debugOutput();
-        }
-        else if (currentCommand.equalsIgnoreCase("test")) {
-            actuator.runSelfTest();
-        }
-        else if (currentCommand.startsWith("set")) {
-             //Expected format: "set <actuator> <voltage>"
-            int firstSpace = currentCommand.indexOf(' ');
-            int secondSpace = currentCommand.indexOf(' ', firstSpace + 1);
-            if (firstSpace == -1 || secondSpace == -1) {
-                Serial.println("Invalid set command. Format: set <actuator> <voltage>");
-            } else{
-                String actStr = currentCommand.substring(firstSpace + 1, secondSpace);
-                String voltStr = currentCommand.substring (secondSpace + 1);
-               int actuatorNum = actStr.toInt();
-                float voltage = voltStr.toFloat();
-
-                if (actuatorNum < 0 || actuatorNum >= NUM_ACTUATORS) {
-                    Serial.println("Invalid actuator number.");
-                }
-                else if (voltage < 0.0 || voltage > MAX_VOLTAGE) {
-                    Serial.println("Voltage out of range (0-5V).");
-                }
-                else {
-                    actuator.actuatorPositions[actuatorNum] = voltage;
-                    actuator.writeDAC(actuatorNum, voltage);
-                    Serial.print("Actuator ");
-                    Serial.print(actuatorNum);
-                    Serial.print(" manually set to ");
-                    Serial.print(voltage);
-                    Serial.print(" V.\n");
-                }
-            }
-        }
-        else if (currentCommand.startsWith("sdo")) {
-          int firstSpace = currentCommand.indexOf(' ');
-          int secondSpace = currentCommand.indexOf(' ', firstSpace + 1);
-          if (firstSpace == -1 || secondSpace == -1) {
-            Serial.println("Invalid sdo commannd. Format: sdo <node id> <index>");
-          } else {
-            String nodeIdStr = currentCommand.substring(firstSpace + 1, secondSpace);
-            String indexStr = currentCommand.substring(secondSpace + 1);
-            
-            uint8_t nodeId = nodeIdStr.toInt();
-            uint16_t index = (uint16_t)strtol(indexStr.c_str(), NULL, 0);
-
-            // Send the SDO read request.
-            Serial.print("SDO read request sent to node ");
-            Serial.print(nodeId, DEC);
-            Serial.print(" for index 0x");
-            Serial.print(index, HEX);
-          }
-        }
-
-        else {
-            Serial.println("Unknown command. Type 'help' for a list of commands.");
-        }
-
-        //Move on to check the next command in the buffer.
-        newlineIndex = commandBuffer.indexOf('\n');
-
-    }
-}
-*/
 
 void setup(void);
 void loop(void);
@@ -201,46 +88,21 @@ void setup() {
   }
   Serial.println("Serial Starting");
 
-/*
-  //  Serial Encoder Testing: Initialize CAN 250kbit
-  if (!CAN.begin(CanBitRate::BR_250k)) {
-    Serial.println("CAN.begin(...) failed!");
-    while(1) {
-      delay(1000);
-    }
-  }
-  Serial.println("CAN interface Initialized.");
-  Serial.println("Type 'help' for command instructions.");
-  //
-*/
-
   delay(2000);
   M7Timer.attachInterruptInterval(100, m7timer);
   mySerial.setup();
   motors.setup();
   ultrasonic.setup();
-  //encoder.setup();
+  encoder.setup();
   actuator.setup ();
-
 }
 
 void loop() {
-  //processSerialCommands();
-  //actuator.handleSerialCommand();  
   //blueLed.stateMachine();
   mySerial.stateMachine();
   motors.stateMachine();
   ultrasonic.stateMachine();
   actuator.stateMachine();
-
-  //encoder.stateMachine();  
-  //encoder.debugOutput();
-
-  // Encoder Serial Test:
-  //while (CAN.available()) {
-  //  CanMsg msg = CAN.read();
-  //  encoder.dumpCanMsg(msg);
-  //  encoder.processCanMessage(msg);
-  //}
-
+  encoder.stateMachine();  
+  //encoder.debugRawInputs();
 }
